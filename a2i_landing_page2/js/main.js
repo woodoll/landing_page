7440cc9865026f356601f288b5647042b178d9b4
woodoll <87690037+woodoll@users.noreply.github.com>
2021-10-14 21:09:52 +0900

    10-14
    다시 만들었다

---
+++ a2i_landing_page2/js/main.js
@@ -0,0 +1,91 @@
+window.addEventListener("wheel", function(e){
+    e.preventDefault();
+},{passive : false});
+
+$(document).ready(function(){
+
+    var mHtml = $("html");
+    var page = 1;
+
+
+    mHtml.animate({scrollTop : 0},10);
+
+    $(window).on("wheel", function(e) {
+        if(mHtml.is(":animated")) return;
+        if(e.originalEvent.deltaY > 0) {
+            if(page == 4) return;
+            page++;
+        } else if(e.originalEvent.deltaY < 0) {
+            if(page == 1) return;
+            page--;
+        }
+        var posTop =(page-1) * $(window).height();
+        mHtml.animate({scrollTop : posTop});
+    });
+
+    $('#btn1').click(function() {
+        $('.num1').css('display', 'block');
+    });
+
+    $('#btn2').click(function() {
+        $('.num2').css('display', 'block');
+    });
+
+    $('#btn3').click(function() {
+        $('.num3').css('display', 'block');
+    });
+
+    $('#btn4').click(function() {
+        $('.num4').css('display', 'block');
+    });
+
+    $('.num1').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.num2').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.num3').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.num4').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.btn_m1').click(function() {
+        $('.num_m1').css('display', 'block');
+    });
+
+    $('.btn_m2').click(function() {
+        $('.num_m2').css('display', 'block');
+    });
+
+    $('.btn_m3').click(function() {
+        $('.num_m3').css('display', 'block');
+    });
+
+    $('.btn_m4').click(function() {
+        $('.num_m4').css('display', 'block');
+    });
+
+    $('.num_m1').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.num_m2').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.num_m3').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    $('.num_m4').click(function() {
+        $(this).css('display', 'none');
+    });
+
+    
+});
